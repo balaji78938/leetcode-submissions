d6821240e098718fab45c53730487f131649a779
balaji78938 <mr.balaji4800@gmail.com>
2026-09-08 13:39:48 +0530

    Time: 0 ms (100%), Space: 19.1 MB (98.86%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,14 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        paths=path.split('/')
+        stack=[]
+        for i in paths:
+            if i=="" or i =="/" or i==".":
+                continue
+            if i=="..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(i)
+
+        return '/'+'/'.join(stack)
