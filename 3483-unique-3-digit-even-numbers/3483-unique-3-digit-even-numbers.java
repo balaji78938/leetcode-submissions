f8d8205740a29db9bc13db308438f30bc484991d
balaji78938 <mr.balaji4800@gmail.com>
2026-09-11 09:07:02 +0530

    Time: 1 ms (100%), Space: 44.4 MB (87.07%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,26 +1,29 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        int[] count = new int[10];
-        for (int d : digits) {
-            count[d]++;
+        int[] cnt = new int[10];
+        for(int digit : digits){
+             cnt[digit]++;
         }
-
-        int total = 0;
-        for (int num = 100; num <= 998; num += 2) {
-            int h = num / 100;
-            int t = (num / 10) % 10;
-            int u = num % 10;
-
-            int[] req = new int[10];
-            req[h]++;
-            req[t]++;
-            req[u]++;
-
-            if (count[h] >= req[h] && count[t] >= req[t] && count[u] >= req[u]) {
-                total++;
+        int ans = 0;
+        for(int i = 1; i <= 9; i++) {
+             if(cnt[i] == 0){
+                continue;
+             }
+             cnt[i]--;
+         for(int j = 0; j <= 9; j++) {
+               if(cnt[j] == 0) {
+                  continue;
+               }
+               cnt[j]--;
+         for(int k : new int[]{0,2,4,6,8}) {
+                 if(cnt[k] > 0) {
+                     ans++;
+                 }
             }
-        }
-
-        return total;
+         cnt[j]++;      
+              }    
+         cnt[i]++;
+             }
+        return ans;
     }
 }
