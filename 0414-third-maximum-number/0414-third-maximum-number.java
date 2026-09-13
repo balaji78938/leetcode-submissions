99d19a62ea7ac616d8d612fd393f4b4d8d00c115
balaji78938 <mr.balaji4800@gmail.com>
2026-09-13 14:57:45 +0530

    Time: 3 ms (63.95%), Space: 44.6 MB (78.27%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,15 +1,24 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        Set<Integer> set=new HashSet<>();
-        for(int i:nums){
-            set.add(i);
+        Long max1 = null;
+        Long max2 = null;
+        Long max3 = null;
+        for (int num : nums) {
+            long n = num;
+            if ((max1 != null && n == max1) || (max2 != null && n == max2) || (max3 != null && n == max3)) {
+                continue;
+            }
+            if (max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (max3 == null || n > max3) {
+                max3 = n;
+            }
         }
-        PriorityQueue<Integer> heap=new PriorityQueue<>(Collections.reverseOrder());
-        heap.addAll(set);
-        if(heap.size()<3)
-            return heap.peek();
-        heap.poll();
-        heap.poll();
-        return heap.peek();
+        return max3 == null ? max1.intValue() : max3.intValue();
     }
 }
