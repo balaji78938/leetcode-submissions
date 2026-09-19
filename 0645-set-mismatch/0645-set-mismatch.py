0bc6a28ecd87aca7f8797d4590a9a2346838eac3
balaji78938 <mr.balaji4800@gmail.com>
2026-09-19 11:23:58 +0530

    Time: 8 ms (77.61%), Space: 20.3 MB (95.68%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,18 +1,13 @@
 class Solution:
     def findErrorNums(self, nums: list[int]) -> list[int]:
-        mini=min(nums)
-        maxi=max(nums)
-        if mini==maxi==1:
-            return[1,2]
-        if mini==maxi:
-            return[mini,maxi-1]
-        res=[0 for i in range(0,maxi+2)]
+        n=len(nums)
+        res=[0]*(n+1)
         for i in nums:
             res[i]+=1
-            rep,miss=0,0
-        for i in range(1,maxi+2):
+        rep,miss=0,0
+        for i in range(1,n+1):
             if res[i]==2:
                 rep=i
-            if miss==0 and res[i]==0:
+            elif res[i]==0:
                 miss=i
         return [rep,miss]
