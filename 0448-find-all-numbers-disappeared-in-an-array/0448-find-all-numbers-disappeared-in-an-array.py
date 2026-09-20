03c48f3733d15ae16368ec22311709b075462b85
balaji78938 <mr.balaji4800@gmail.com>
2026-09-20 12:24:15 +0530

    Time: 16 ms (97.05%), Space: 31.4 MB (16.48%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -4,4 +4,4 @@ class Solution:
         count=[0]*(n+1)
         for i in nums:
             count[i]=1
-        return [i for i in range(1,n+1) if count[i]<1]
+        return [i for i in range(1,n+1) if count[i]==0]
